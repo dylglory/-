d1049d99b6c5995a83bdfdf274cfd7cf4d864824
dylglory <116704723+dylglory@users.noreply.github.com>
2022-10-27 23:58:04 +0800

    Add files via upload

---
+++ 三个整数的大中小数.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,temp;
+    scanf("%d%d%d",&a,&b,&c);
+    if (a>b)
+    {
+        temp=b;
+        b=a;
+        a=temp;
+    }
+    if(a>c)
+    {
+        temp=c;
+        c=a;
+        a=temp;
+    }
+    if(b>c)
+    {
+       temp=c;
+       c=b;
+       b=temp;
+    }
+    printf("Max=%d\nMin=%d\nMid=%d\n",c,a,b);
+    return 0;
+    }
+
